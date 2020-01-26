fa23d9dad8b54dfc39f0219a209d64f699ae2b09
TSUTSUI Kazuo <kzt206@gmail.com>
2020-01-26 14:45:52 +0900

    Test of printwriter and gitignore

--- src/suiri2_1/Test.java
+++ src/suiri2_1/Test.java
@@ -1,5 +1,13 @@
 package suiri2_1;
 
-public class Test {
+import java.io.PrintWriter;
 
+public class Test {
+	public static void main(String... args) {
+		try(PrintWriter pWriter = new PrintWriter("data/test2.txt")){
+			pWriter.println("test");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
