5a4e3e1d0afaf4ce15602068928ba9c8e1d16a8c
kzt206 <kzt206@gmail.com>
2020-02-03 19:12:57 +0900

    Codeing page2

--- src/suiri2_1/Main.java
+++ src/suiri2_1/Main.java
@@ -19,7 +19,7 @@ public class Main {
 		double[] ELBPR = new double[NN];
 		double[] DBDXPR = new double[NN];
 		
-		//繝代Λ繝｡繝ｼ繧ｿ縺ｮ險ｭ螳�
+		//Setting parameters
 		double XL1 = 0.12;
 		double XL2 = 0.12;
 		double XL3 = 0.12;
@@ -31,6 +31,8 @@ public class Main {
 		double DG = 9.8;
 		double DX = 0.005;
 		
+		
+		//Number of grids
 		double AMP = 10000.1;
 		int IXL1 = (int) (XL1*AMP);
 		int IXL2 = (int) (XL2*AMP);
@@ -49,8 +51,27 @@ public class Main {
 			e.printStackTrace();
 		}
 		
+		//限界水深の計算 : HC
+		double HC = Math.pow((Math.pow(Q, 2.)/DG/ANGC),0.333333);
 		
+		//特異点位置の計算 : XS
+		double S1 = Math.pow(AN*Q,2.)/Math.pow(HC, 3.333333);
+		double S2 = Math.pow((S1-ANGS)/ANGC, 2.);
+		double XS = XL1 + XL2*0.5 + S2*Math.pow(R0, 2.)/(1.0 + S2);
 		
+		//特定点（鞍形点）位置の水面勾配
+		double S3 = 3.333333 * Math.pow(AN*Q,2.)/Math.pow(HC, 4.3333333);
+		double S4 = 3.0*Math.pow(Q, 2.)/DG/Math.pow(HC, 4.);
+		double D2 = -1. * Math.pow(R0, 2.)/Math.pow((Math.pow(R0, 2.)-Math.pow(XS-XL1-XL2*0.5, 2.)),1.5);
+		double DHS1 = (S3 + Math.pow(Math.pow(S3, 2.)-4.*ANGC*D2*S4, 0.5))/2.0/S4;
+		double DHS2 = (S3 - Math.pow(Math.pow(S3, 2.)-4.*ANGC*D2*S4, 0.5))/2.0/S4;
+		
+		//*******************************************************************
+		//     特異点から下流への水面形追跡
+		//*******************************************************************
+		
+		//IINI : 上流端から数えた特異点直下流の格子番号
+		int IINI = (int)(XS/DX) + 2;
 		
 	}
 }
